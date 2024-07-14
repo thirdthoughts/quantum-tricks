7cdb558d8f1e8a142cc7d06955daf8d0c74dbe7d
jnewquist <83454561+jnewquist@users.noreply.github.com>
2024-07-14 15:34:46 -0400

    more flavor icon cleanup

--- src/_util/constants.ts
+++ src/_util/constants.ts
@@ -14,4 +14,3 @@ export const flavors = [
   "Bottom",
   "Top",
 ] as const;
-
--- src/app/viewLobby/_components/LobbyEmptyPlayerView.tsx
+++ src/app/viewLobby/_components/LobbyEmptyPlayerView.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import {
-  BottomFlavorIcon,
-  CharmFlavorIcon,
-  DownFlavorIcon,
-  StrangeFlavorIcon,
-  TopFlavorIcon,
-  UpFlavorIcon,
+  FlavorIcon,
 } from "~/_svg/flavorIcons";
 
 export default function LobbyEmptyPlayerView() {
@@ -15,12 +10,12 @@ export default function LobbyEmptyPlayerView() {
       <div className="flex w-1/12 bg-slate-200 p-1"></div>
       <div className="flex w-1/2 bg-slate-200 p-1">Empty</div>
       <div className="flex w-1/3 bg-slate-200 p-1">
-        <TopFlavorIcon></TopFlavorIcon>
-        <StrangeFlavorIcon></StrangeFlavorIcon>
-        <BottomFlavorIcon></BottomFlavorIcon>
-        <CharmFlavorIcon></CharmFlavorIcon>
-        <UpFlavorIcon></UpFlavorIcon>
-        <DownFlavorIcon></DownFlavorIcon>
+        <FlavorIcon flavor="Top"></FlavorIcon>
+        <FlavorIcon flavor="Bottom"></FlavorIcon>
+        <FlavorIcon flavor="Charm"></FlavorIcon>
+        <FlavorIcon flavor="Strange"></FlavorIcon>
+        <FlavorIcon flavor="Up"></FlavorIcon>
+        <FlavorIcon flavor="Down"></FlavorIcon>
       </div>
     </div>
   );
