ac26cffe1aecec5175b1586bef4a2e9c2108c4b7
jnewquist <83454561+jnewquist@users.noreply.github.com>
2024-07-19 15:21:32 -0400

    quick link back to home page from lobby

--- src/app/viewLobby/[slug]/page.tsx
+++ src/app/viewLobby/[slug]/page.tsx
@@ -5,6 +5,7 @@ import LobbyPlayer from "../_components/LobbyPlayer";
 import LobbyJoiner from "../_components/LobbyJoiner";
 import { JoinLobby, LeaveLobby } from "~/server/actions";
 import { flavors } from "~/_util/constants";
+import Link from "next/link";
 
 export default async function ViewLobby({
   params,
@@ -15,42 +16,53 @@ export default async function ViewLobby({
   const usedFlavors = gameLobby.players.map((p) => p.playerFlavor);
   const availableFlavors = flavors.filter((f) => !usedFlavors.includes(f));
   return (
-    <div className="flex max-w-lg flex-col gap-1">
-      <div className="flex flex-row gap-1">
-        <div className="flex w-1/12 rounded-tl-lg bg-slate-500 p-1"></div>
-        <div className="flex w-1/2 bg-slate-500 p-1">Player</div>
-        <div className="flex w-1/3 bg-slate-500 p-1">Flavor</div>
-        <div className="flex w-1/6 rounded-tr-lg bg-slate-500 p-1">Action</div>
-      </div>
-      {gameLobby.players.map((p) => (
-        <LobbyPlayer
-          player={p}
-          key={`${p.playerFlavor}_player`}
-          myGame={gameLobby.mine}
-          Leave={LeaveLobby}
-          params={params}
-        ></LobbyPlayer>
-      ))}
-      <LobbyJoiner
-        Join={JoinLobby}
-        params={params}
-        alreadyIn={gameLobby.alreadyIn}
-        availableFlavors={availableFlavors}
-      ></LobbyJoiner>
-      {Array.from({
-        length: gameLobby.playerCount - gameLobby.players.length - 1,
-      }).map((it, index) => {
-        return (
-          <div className="flex flex-row gap-1" key={index}>
-            <div className="flex w-1/12 bg-slate-400 p-1"></div>
-            <div className="flex w-1/2 bg-slate-400 p-1">Open</div>
-            <div className="flex w-1/3 bg-slate-400 p-1"></div>
-            <div className="flex w-1/6 bg-slate-400 p-1"></div>
+    <>
+      <Link
+        className="flex w-24 cursor-pointer select-none items-center justify-center rounded-lg border-2
+     border-gray-800 bg-green-800 p-1"
+        href="/"
+      >
+        Home
+      </Link>
+      <div className="h-4"></div>
+      <div className="flex max-w-xl flex-col gap-1">
+        <div className="flex flex-row gap-1">
+          <div className="flex w-1/12 rounded-tl-lg bg-slate-500 p-1"></div>
+          <div className="flex w-1/2 bg-slate-500 p-1">Player</div>
+          <div className="flex w-1/3 bg-slate-500 p-1">Flavor</div>
+          <div className="flex w-1/6 rounded-tr-lg bg-slate-500 p-1">
+            Action
           </div>
-        );
-      })}
-    </div>
+        </div>
+        {gameLobby.players.map((p) => (
+          <LobbyPlayer
+            player={p}
+            key={`${p.playerFlavor}_player`}
+            myGame={gameLobby.mine}
+            Leave={LeaveLobby}
+            params={params}
+          ></LobbyPlayer>
+        ))}
+        <LobbyJoiner
+          Join={JoinLobby}
+          params={params}
+          alreadyIn={gameLobby.alreadyIn}
+          availableFlavors={availableFlavors}
+        ></LobbyJoiner>
+        {Array.from({
+          length: gameLobby.playerCount - gameLobby.players.length - 1,
+        }).map((it, index) => {
+          return (
+            <div className="flex flex-row gap-1" key={index}>
+              <div className="flex w-1/12 bg-slate-400 p-1"></div>
+              <div className="flex w-1/2 bg-slate-400 p-1">Open</div>
+              <div className="flex w-1/3 bg-slate-400 p-1"></div>
+              <div className="flex w-1/6 bg-slate-400 p-1"></div>
+            </div>
+          );
+        })}
+      </div>
+    </>
     //TODO if player already in game, allow changing flavor
-    //TODO quick link back to home page/lobby list
   );
 }
